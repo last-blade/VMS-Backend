eccef69d7f002b021e5052c72c2c9db44b63c34c
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 09:21:33 +0530

    remove unnecessary if condition

--- src/utils/generateRefreshToken.js
+++ src/utils/generateRefreshToken.js
@@ -1,13 +1,6 @@
-import { Employee } from "../models/employee.model.js";
 import { User } from "../models/user.model.js"
 
 const generateRefreshToken = async (userId, accountType) => {
-
-    if(accountType === "Supervisor" || accountType === "Mechanic" || accountType === "Production"){
-        const foundUser = await Employee.findById(userId);
-        const refreshToken = foundUser.generateRefreshToken();
-        return refreshToken;
-    }
     const foundUser = await User.findById(userId);
 
     const refreshToken = await foundUser.generateRefreshToken();
