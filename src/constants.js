3ccbb5343d053b21e706e963dce28c19c719b9c2
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 17:45:55 +0530

    add token option

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,13 @@
 export const DB_NAME = "VMS-SRPK"
+export const accessTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 *1000,
+}
+export const refreshTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 28 * 24 * 60 * 60 *1000,
+}
