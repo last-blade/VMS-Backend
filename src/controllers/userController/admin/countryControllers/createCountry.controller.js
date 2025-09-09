e79ca60fa027097580bde4089bc0943b0adbed34
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 13:24:58 +0530

    add createCountry controller

---
+++ src/controllers/userController/admin/countryControllers/createCountry.controller.js
@@ -0,0 +1,33 @@
+import { apiError, apiResponse, asyncHandler, Country } from "../../../allImports.js";
+
+const createCountry = asyncHandler(async (request, response) => {
+    const {countryName} = request.body;
+
+    if(countryName.trim() === "" || countryName === undefined){
+        throw new apiError(400, "Country name is required")
+    }
+
+    const foundCountry = await Country.findOne({
+        countryName,
+        countryCreator: request.user.id,
+        company: request.user.company,
+    });
+
+    if(foundCountry){
+        throw new apiError(400, "Country name already exists")
+    }
+
+    await Country.create({
+        countryName,
+        countryCreator: request.user.id,
+        company: request.user.company,
+    });
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, {}, "Country created successfully")
+    )
+
+});
+
+export {createCountry}
