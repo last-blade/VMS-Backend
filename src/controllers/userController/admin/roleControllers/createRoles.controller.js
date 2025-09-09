f0425d60081390a35e118c7a9b223b1c12d5feaa
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 13:26:52 +0530

    add check for duplicate roles creation will throw error

--- src/controllers/userController/admin/roleControllers/createRoles.controller.js
+++ src/controllers/userController/admin/roleControllers/createRoles.controller.js
@@ -7,6 +7,16 @@ const createRoles = asyncHandler(async (request, response) => {
         throw new apiError(400, "Role name required")
     }
 
+    const foundRole = await Role.findOne({
+        roleName,
+        roleCreator: request.user.id,
+        company: request.user.company,
+    });
+
+    if(foundRole){
+        throw new apiError(400, "Role name already exists")
+    }
+
     await Role.create({
         roleName,
         roleCreator: request.user.id,
