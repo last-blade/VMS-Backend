2786445d839e382ac0523f22e725a3e7f0d10a65
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 09:48:01 +0530

    add editRoles controller

---
+++ src/controllers/userController/admin/roleControllers/editRoles.controller.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { apiError, apiResponse, asyncHandler, isObjectIdValid, Role } from "../../../allImports.js";
+
+const editRoles = asyncHandler(async (request, response) => {
+    const {roleId} = request?.params;
+    const {roleName} = request.body;
+
+    if(!roleId){
+        throw new apiError(400, "Role id required")
+    }
+
+    if(!isObjectIdValid(roleId)){
+        throw new apiError(400, "Role ID is invalid")
+    }
+
+    if(roleName.trim() === "" || roleName === undefined){
+        throw new apiError(400, "Role name required")
+    }
+
+    await Role.findByIdAndUpdate(roleId, {
+        $set: {
+            roleName
+        }
+    }, {new: true});
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Role name updated successfully")
+    )
+
+});
+
+export {editRoles}
