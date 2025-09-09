425d304594243484ebde9f0e16d16a2cf6a6fd4e
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 09:47:51 +0530

    add create and edit role routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,18 @@ import { registerUser } from "../controllers/userController/registerUser.control
 import { loginUser } from "../controllers/userController/loginUser.controller.js";
 import { logoutUser } from "../controllers/userController/logoutUser.controller.js";
 import { authentication } from "../middlewares/auth.middleware.js";
+import { createRoles } from "../controllers/userController/admin/roleControllers/createRoles.controller.js";
+import { editRoles } from "../controllers/userController/admin/roleControllers/editRoles.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(authentication, logoutUser);
+
+
+//Role routes
+router.route("/role/create-role").post(authentication, createRoles);
+router.route("/role/edit-role/:roleId").patch(authentication, editRoles);
 
 export default router;
