10e90714ef9a01d81341cac4fd5a569c5c3f392f
last-blade <meprashanttyagi2000@gmail.com>
2025-09-09 09:21:19 +0530

    add login, logout routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/userController/registerUser.controller.js";
+import { loginUser } from "../controllers/userController/loginUser.controller.js";
+import { logoutUser } from "../controllers/userController/logoutUser.controller.js";
+import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(authentication, logoutUser);
 
 export default router;
