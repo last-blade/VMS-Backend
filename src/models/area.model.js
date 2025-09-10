a04a7cc27541965cd4e4a60ade5f95f020966db1
last-blade <meprashanttyagi2000@gmail.com>
2025-09-10 17:28:16 +0530

    add area model for the company's areas

---
+++ src/models/area.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const areaSchema = new Schema({
+    areaName: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+
+    plant: {
+        type: Schema.Types.ObjectId,
+        ref: "Plant",
+        required: true,
+    },
+
+    company: {
+        type: Schema.Types.ObjectId,
+        ref: "Company",
+        required: true,
+        index: true,
+    },
+
+    areaCreator: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
+    },
+
+}, {timestamps: true});
+
+// Compound unique index
+areaSchema.index({ plant: 1, areaName: 1 }, { unique: true }); 
+//Same plant cannot have two areas with the same name
+//Different plants can have areas with the same name
+
+export const Area = mongoose.model("Area", areaSchema);
