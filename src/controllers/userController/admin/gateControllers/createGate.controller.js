dbb13ad3083b5425cffa94eb5d2906ff7abae563
last-blade <meprashanttyagi2000@gmail.com>
2025-09-10 14:22:38 +0530

    add createGate controller to create entry gates of the company

---
+++ src/controllers/userController/admin/gateControllers/createGate.controller.js
@@ -0,0 +1,47 @@
+import { apiError, apiResponse, asyncHandler, Gate, isObjectIdValid } from "../../../allImports.js";
+
+const createGate = asyncHandler(async (request, response) => {
+    const {gateName, gateNumber, gateInchargeName, company, plant, gateOpenTime, gateCloseTime, gateSecurity} = request.body;
+
+    if(gateName.trim() === "" || gateName === undefined){
+        throw new apiError(400, "Gate name required")
+    }
+
+    if(gateNumber.trim() === "" || gateNumber === undefined){
+        throw new apiError(400, "Gate number required")
+    }
+
+    if([gateInchargeName, company, plant, gateSecurity].some(inputField => !isObjectIdValid(inputField))){
+        throw new apiError(400, "Object IDs are not valid")
+    }
+
+    const foundGate = await Gate.findOne({
+        gateNumber,
+        company: request.user.id,
+        plant,
+    });
+
+    if(foundGate){
+        throw new apiError(400, "This gate number already exists")
+    }
+
+    await Gate.create({
+        gateName,
+        gateNumber,
+        gateInchargeName,
+        company: request.user.company,
+        plant,
+        gateOpenTime,
+        gateCloseTime,
+        gateSecurity,
+        gateCreator: request.user.id,
+    });
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, {}, "Gate created successfully")
+    )
+
+});
+
+export {createGate}
