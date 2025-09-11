b5c0354fdd7448a4ce4535eb058bbe2d8e696bc5
last-blade <meprashanttyagi2000@gmail.com>
2025-09-11 11:32:46 +0530

    checking with both mobile and email if user already exists

--- src/controllers/userController/registerUser.controller.js
+++ src/controllers/userController/registerUser.controller.js
@@ -3,14 +3,16 @@ import { apiError, apiResponse, User, asyncHandler } from "../allImports.js";
 const registerUser = asyncHandler(async (request, response) => {
     const {fullname, email, password, department, mobile, company, plant, address, role} = request.body;
 
-    if([fullname, email, password, department, mobile, company, plant, role].some(inputField => inputField === undefined || inputField.trim === "")){
+    if([fullname, password, department, mobile, company, plant, role].some(inputField => inputField === undefined || inputField.trim === "")){
         throw new apiError(400, "All fields are required")
     }
 
-    const isUserExists = await User.findOne({email});
+    const isUserExists = await User.findOne({
+        $or: [{email}, {mobile}]
+    });
 
     if(isUserExists){
-        throw new apiError(400, "User with this email already exists")
+        throw new apiError(400, "User with this mobile or email already exists")
     }
 
     await User.create({
