03f5b8966825286749e9307e3f77205d254d846d
last-blade <meprashanttyagi2000@gmail.com>
2025-09-12 16:54:38 +0530

    add rejectOrApproveVisitorAppointment controller to manage appointment status

---
+++ src/controllers/userController/appointmentControllers/rejectOrApproveVisitorAppointment.controller.js
@@ -0,0 +1,30 @@
+import { apiError, apiResponse, Appointment, asyncHandler, isObjectIdValid } from "../../allImports.js";
+
+const rejectOrApproveVisitorAppointment = asyncHandler(async (request, response) => {
+    const {appointmentStatus, appointmentId} = request.body;
+
+    if(!appointmentStatus || appointmentStatus.trim() === undefined){
+        throw new apiError(400, "Approval or rejection status is required")
+    }
+
+    if(!isObjectIdValid(appointmentId)){
+        throw new apiError(400, "Appointment ID is invalid")
+    }
+
+    const foundAppointment = await Appointment.findById(appointmentId);
+
+    if(!foundAppointment){
+        throw new apiError(404, "Apointment not found")
+    }
+
+    foundAppointment.isAppointmentActive = appointmentStatus;
+    foundAppointment.save({validateBeforeSave: false});
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, `Appointment ${appointmentStatus}`)
+    )
+
+});
+
+export {rejectOrApproveVisitorAppointment}
