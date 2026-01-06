a79b341d8acaaf7d536db0d2fbe60e9dfcd7d204
last-blade <meprashanttyagi2000@gmail.com>
2026-01-06 15:05:55 +0530

    sending base url with plant and company id

--- src/controllers/userController/admin/plantControllers/createPlant.controller.js
+++ src/controllers/userController/admin/plantControllers/createPlant.controller.js
@@ -53,11 +53,12 @@ const createPlant = asyncHandler(async (request, response) => {
     await Company.findByIdAndUpdate(request.user.company, { $inc: { usageCount: 1 } });
 
     const createdPlant = await Plant.findById(newPlant._id);
-
+    const baseUrl = "https://vms-frontend-pied.vercel.app/visitorform";
     const qrData = JSON.stringify({
-        plantId: newPlant._id,
-        companyId: newPlant.company,
-        url: "https://vms-frontend-pied.vercel.app/visitorform",
+        // plantId: newPlant._id,
+        // companyId: newPlant.company,
+        // url: "https://vms-frontend-pied.vercel.app/visitorform",
+        url: `${baseUrl}?plantId=${newPlant._id}&companyId=${newPlant.company}`
     });
 
     const qrCodeDataUri = await QRCode.toDataURL(qrData);
