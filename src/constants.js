5fb91a8e2fafb9c650611c26613a9ed460d94da2
last-blade <meprashanttyagi2000@gmail.com>
2026-01-16 15:44:47 +0530

    change constants

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,12 @@ export const DB_NAME = "VMS-SRPK"
 export const accessTokenOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "None",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 *1000,
 }
 export const refreshTokenOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "None",
+    sameSite: "none",
     maxAge: 28 * 24 * 60 * 60 *1000,
 }
