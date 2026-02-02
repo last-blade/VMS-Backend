b7fdc1ddd74464e44d249c0ed47ce3c2f4f03d48
last-blade <meprashanttyagi2000@gmail.com>
2026-02-02 10:14:10 +0530

    fixed logic

--- src/controllers/userController/loginUser.controller.js
+++ src/controllers/userController/loginUser.controller.js
@@ -4,56 +4,52 @@ import { generateRefreshToken } from "../../utils/generateRefreshToken.js";
 import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 
 const loginUser = asyncHandler(async (request, response) => {
-    const {email, mobile, password} = request.body;
+  const { email, mobile, password } = request.body;
 
-    if((!email || !email.trim() === "") && !mobile){
-        throw new apiError(400, "Email or mobile number is required for login")
-    }
+  if ((!email || email.trim() === "") && !mobile) {
+    throw new apiError(400, "Email or mobile number is required for login");
+  }
 
-    if(!password){
-        throw new apiError(404, "Password is required")
-    }
+  if (!password) {
+    throw new apiError(400, "Password is required");
+  }
 
-    const foundUser = await User.findOne({
-        $or: [{email}, {mobile}]
-    }).select("+password");
+  const query = {};
+  if (email && email.trim() !== "") query.email = email;
+  if (mobile) query.mobile = Number(mobile);
 
-    if(!foundUser){
-        throw new apiError(404, "User with this email does not exists")
-    }
-
-    const isValidPassword = await foundUser.isPasswordCorrect(password);
-
-    if(!isValidPassword){
-        throw new apiError(401, "Incorrect password")
-    }
-
-    const accessToken = await generateAccessToken(foundUser._id);
-    const refreshToken = await generateRefreshToken(foundUser._id);
-
-    if(!accessToken || !refreshToken){
-        throw new apiError(500, "Error in generating access and refresh token")
-    }
-
-    foundUser.refreshToken = refreshToken;
-    foundUser.save({validateBeforeSave: false});
-
-    const user = await User.findOne({
-        $or: [{email}, {mobile}]
-    })
+  const foundUser = await User.findOne(query)
+    .select("+password")
     .populate("department", "departmentName")
     .populate("company", "companyName")
     .populate("plant", "plantName")
-    .populate("role", "roleName")
-    .select("-password -refreshToken -_id -__v");
+    .populate("role", "roleName");
 
-    return response.status(200)
+  if (!foundUser) {
+    throw new apiError(404, "User not found");
+  }
+
+  const isValidPassword = await foundUser.isPasswordCorrect(password);
+  if (!isValidPassword) {
+    throw new apiError(401, "Incorrect password");
+  }
+
+  const accessToken = await generateAccessToken(foundUser._id);
+  const refreshToken = await generateRefreshToken(foundUser._id);
+
+  foundUser.refreshToken = refreshToken;
+  await foundUser.save({ validateBeforeSave: false });
+
+  foundUser.password = undefined;
+  foundUser.refreshToken = undefined;
+  foundUser.__v = undefined;
+
+  return response
+    .status(200)
     .cookie("accessToken", accessToken, accessTokenOptions)
     .cookie("refreshToken", refreshToken, refreshTokenOptions)
-    .json(
-        new apiResponse(200, user, "Logged in successfully")
-    )
-
+    .json(new apiResponse(200, foundUser, "Logged in successfully"));
 });
+
 
 export {loginUser}
